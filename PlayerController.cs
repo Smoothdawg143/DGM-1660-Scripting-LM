4c2dbcbff5bae85ab9cd98f1532f6065b48c3774
Smoothdawg143 <84424606+Smoothdawg143@users.noreply.github.com>
2021-09-15 11:41:17 -0600

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.5f;
+    public float turnSpeed;
+
+    public float hInput; 
+
+    public float vInput; 
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        hInput = Input.GetAxis("Horizontal");
+        vInput = Input.GetAxis("Vertical");
+        // Move the tank left and right
+        transform.Rotate(Vector3.up, turnSpeed * hInput * Time.deltaTime);
+        // Moves the tank forward and back
+        transform.Translate(Vector3.forward * speed * Time.deltaTime * vInput);
+    }
+}
